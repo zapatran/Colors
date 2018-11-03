d57da7aeecdc4b12f4a36f9669796b5fb0cd7775
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2018-11-03 16:59:37 +0100

    change class component to function component

--- src/components/CardList/CardList.js
+++ src/components/CardList/CardList.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from '../Card/Card';
 
 import './CardList.css'
 
-class CardList extends Component {
-  render () {
-    const { data } = this.props;
-
-    return (
-      <div className="wrapper">
-        { data.map((element, index) => <Card key={`${index}-${element[0]}`} from={element[0]} to={element[1]} /> )}
-      </div>
-    )
-  }
+const CardList = (props) => {
+  const { data } = props;
+  return (
+    <div className="wrapper">
+      { data.map((element, index) => <Card key={`${index}-${element[0]}`} from={element[0]} to={element[1]} /> )}
+    </div>
+  );
 }
 
 export default CardList;
