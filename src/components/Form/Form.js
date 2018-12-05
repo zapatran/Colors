fcfc4eb8ed7cbb2859df1ea489b327adfc4a7b96
Thor Jubera Albo <thor.jubera.albo@gmail.com>
2018-12-05 17:44:54 +0100

    refactor

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,31 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Form.css';
 
-const Form = (props) => {
-  return (
-    <form className="form" onSubmit={(e) => {
-      e.preventDefault();
-      props.onSubmit(this._from.value, this._to.value);
-    }}>
-      <fieldset>
-        <label>From:</label>
-        <input 
-          defaultValue=''
-          type="text"
-          placeholder="#AABBCC"
-          ref={ input => this._from = input }
-        />
-        <label>To:</label>
-        <input 
-          defaultValue=''
-          type="text"
-          placeholder="#AABBCC"
-          ref={ input => this._to = input }
-        />
-        <input type="submit" className="block" />
-      </fieldset>
-    </form>
-  )
+class Form extends Component {
+  state = {
+    from: '',
+    to: '',
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+
+  render() {
+    const { from, to } = this.state;
+    return <form className="form" onSubmit={e => {
+          e.preventDefault();
+          this.props.onSubmit(from, to);
+        }}>
+        <fieldset>
+          <label>From:</label>
+          <input defaultValue="" type="text" placeholder="#AABBCC" name="from" value={from} onChange={this.handleChange} />
+          <label>To:</label>
+          <input defaultValue="" type="text" placeholder="#AABBCC" name="to" value={to} onChange={this.handleChange} />
+          <input type="submit" className="block" />
+        </fieldset>
+      </form>;
+  }
 }
 
 export default Form;
